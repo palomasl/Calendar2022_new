312c26be6a6b8f7a2d4b4a88f9ac2a37b3c06285
unknown <paloma.sanl@gmail.com>
2022-01-17 00:09:47 +0000

    insert doors

--- js/script.js
+++ js/script.js
@@ -3,32 +3,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
-const jsonData = require('./data/months.json'); 
-console.log(jsonData);
-
-/*createDoors = function() {
-  var doors = "";
-    for (var i = 0; i < months.months.length; i++) {
-        if (i == 0) {
-            doors += "<section class=\"row\">"
-        }
-        if (i % 3 == 0 && i != 0) {
-            doors += "</section>" + "<br>" + "<section class=\"row\">"
-        }
-        doors += "<div class=\"col-12 col-sm-4\">" +
-            "<button class=\"card text-center\">" +
-            "<div class=\"door\">" + 
-            "<div class=\"month\">" + 
-            months.months[i].name + 
-            "</div>" +
-            "</div>" + 
-            "</button>" 
-            + "</div>";
-        if (i == 11) {
-            doors += "</section>"
-        }
-    }
-    console.log(doors);
-    $("#doors").html(doors);  
-}
-*/
+//Load JSON
+document.addEventListener("DOMContentLoaded",
+        function() {
+            $ajaxUtils
+                .sendGetRequest("data/months.json",
+                    function(months) {
+                        var doors = "";
+                        for (var i = 0; i < months.months.length; i++) {
+                            if (i == 0) {
+                                doors += "<section class=\"row\">"
+                            }
+                            if (i % 3 == 0 && i != 0) {
+                                doors += "</section>" + "<br>" + "<section class=\"row\">"
+                            }
+                            doors += "<div class=\"col-12 col-sm-4\">" +
+                                "<button>" +
+                                "<div class=\"door\">" +
+                                "<div class=\"month\">" +
+                                months.months[i].name +
+                                "</div>" +
+                                "</div>" +
+                                "</button>" +
+                                "</div>";
+                            if (i == 11) {
+                                doors += "</section>"
+                            }
+                        }
+                        console.log(doors);
+                        $("#doors").html(doors);
+                    })
+                });
