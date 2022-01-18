4eee41aec29d8f3e3904404a9558d993f1ed3c87
unknown <paloma.sanl@gmail.com>
2022-01-18 22:24:26 +0000

    closeDoor function

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded",
                                 doors += "</section>" + "<br>" + "<section class=\"row\">"
                             }
                             doors += "<div class=\"col-12 col-sm-4\">" +
-                                "<button style=\"background-image: url('./images/" + createdoors.months[i].image + "'); background-size: cover\">" +
-                                "<div class=\"door\" onmouseenter=\"openDoor()\" onmouseleave=\"closeDoor()\">" +
+                                "<button onmouseleave=\"closeDoor()\" style=\"background-image: url('./images/" + createdoors.months[i].image + "'); background-size: cover\">" +
+                                "<div class=\"door\" onmouseenter=\"openDoor()\">" +
                                 "<div class=\"month\">" +
                                 createdoors.months[i].name +
                                 "</div>" +
@@ -41,8 +41,16 @@ var openDoor = function(){
     var curmonth = curdate.toLocaleString('en-US', {month: 'long'});
     var doormonth = event.target.innerText;
     if(curmonth == doormonth){
-    event.target.style.transform = "perspective(1000px) translateX(-70px) rotateY(-100deg)"; 
-    event.target.style.boxShadow = "none";
-    event.target.style.borderColor = "5px solid #363636";
+    $(event.target).css("transform","perspective(1000px) translateX(-70px) rotateY(-100deg)"); 
+    $(event.target).css("boxShadow", "none");
+    $(event.target).css("borderColor", "5px solid #363636");
 }};
 //-- END-Open doors
+
+//Close doors
+var closeDoor = function(){
+    $(event.target).children().css("transform","perspective(0px) translateX(0px) rotateY(0deg)"); 
+    $(event.target).children().css("boxShadow", "2px 2px 5px #fff");
+    $(event.target).children().css("borderColor", "5px solid #fff");
+};
+//-- END-Close doors
